3022ffa92fc2b3ef5c6fb452f8ef0b8cc7fc9912
chiket <limchiket@hotmail.com>
2025-01-06 22:08:54 +0800

    Update random_url.js

--- random_url.js
+++ random_url.js
@@ -1,4 +1,4 @@
 var url = new Array(); 
 url[0] = 'https://www.amazon.com/';
-url[1] = 'https://www.yahoo.co.jp/'; 
+url[1] = 'https://www.yahoo.com/'; 
 location.assign(url[Math.round(Math.random()*(url.length-1))]);
